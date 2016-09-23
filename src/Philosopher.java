3cf92dd074239f3655b3238bd56fa5567ff4da76
Daniel <dds2130@rit.edu>
2016-09-23 11:44:52 -0400

    think and releaseForks

--- src/Philosopher.java
+++ src/Philosopher.java
@@ -1,4 +1,5 @@
 package src;
+import java.util.concurrent.ThreadLocalRandom;
 import java.lang.*;
 
 public class Philosopher extends Thread {
@@ -21,7 +22,33 @@ public class Philosopher extends Thread {
 	}
 
 	public void run() {
+		boolean infinite = (nTimes == 0);
 
+		do {
+			think();
+
+			releaseForks();
+
+			nTimes--;
+		} while (infinite || nTimes > 0);
 	}
 
+
+	private void think() {
+		ThreadLocalRandom random = ThreadLocalRandom.current();
+		long t = random.nextLong(thinkMillis + 1);
+		System.out.println("Philosopher " + id + " thinks for " +
+			t + " milliseconds");
+		try {
+			Thread.sleep(t);
+		}
+		catch(InterruptedException e) {}
+	}
+
+	private void releaseForks() {
+		right.release();
+		System.out.println("Philosopher " + id + "releases right fork");
+		left.release();
+		System.out.println("Philosopher " + id + "releases left fork");
+	}
 }
