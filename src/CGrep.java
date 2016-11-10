5bc816ae9ed9a860922151135e490764d7918212
Daniel <dds2130@rit.edu>
2016-11-10 13:01:44 -0500

    define classes

--- src/CGrep.java
+++ src/CGrep.java
@@ -1,1 +1,13 @@
 package src;
+
+public class CGrep {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.err.println("Not enough arguments");
+            System.exit(1);
+        }
+
+
+        
+    }
+}
