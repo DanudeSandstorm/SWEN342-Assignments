9a1da77eb0236c3c8c60accc7f3a4f42e11e6161
Daniel <dds2130@rit.edu>
2016-11-11 18:18:48 -0500

    onfound

--- src/CollectionActor.java
+++ src/CollectionActor.java
@@ -1,8 +1,8 @@
 package src;
 
+import akka.actor.Actors;
 import akka.actor.UntypedActor;
-
-import java.io.File;
+import java.util.List;
 
 public class CollectionActor extends UntypedActor {
     private int filesScanned;
@@ -27,10 +27,39 @@ public class CollectionActor extends UntypedActor {
         }
     }
 
+    /**
+     * The first, of class FileCount (which should be received exactly once),
+     * contains a count of the number of files being scanned.
+     */
     private void setFileCount(FileCount message) {
         filesToScan = message.getCount();
     }
 
-    private void onFound(Found found) {}
+    /**
+     * The remaining messages are Found objects, which, upon receipt,
+     * are printed by the CollectionActor.
+     * Printout consists of the file name (or "-" for standard input)
+     * and the list of matching lines.
+     */
+    private void onFound(Found found) {
+        List<String> matches = found.getMatches();
+        if (matches.size() == 0 ) {
+            System.out.println("No matches for " + found.getName());
+            return;
+        }
+        System.out.println("Matches for: " + found.getName());
+        for (String match : matches) {
+            System.out.println(match);
+        }
+
+        filesScanned++;
+
+        // When all the Found messages have been processed, the CollectionActor
+        // shuts down all actors using Actors.registry().shutdown()
+        if (filesScanned == filesToScan) {
+            Actors.registry().shutdownAll();
+        }
+    }
+
 
 }
