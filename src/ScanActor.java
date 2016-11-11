a8d6ef2b779d7b1145cadf7665f383889f0fb79d
Daniel <dds2130@rit.edu>
2016-11-11 16:35:42 -0500

    basic object and message receiving

--- src/ScanActor.java
+++ src/ScanActor.java
@@ -1,4 +1,44 @@
 package src;
 
-public class ScanActor {
+import akka.actor.ActorRef;
+import akka.actor.UntypedActor;
+import java.util.regex.*;
+
+public class ScanActor extends UntypedActor {
+    private ActorRef collection;
+    private String filePath;
+    private final String regex;
+    private boolean configured;
+
+    public ScanActor(String regex) {
+        this.regex = regex;
+        configured = false;
+    }
+
+    @Override
+    public void onReceive(Object message) throws Exception {
+
+        if (message instanceof Configure) {
+            if (configured) {
+                throw new RuntimeException("Actor is already configured");
+            }
+            configure((Configure) message);
+        }
+
+    }
+
+    private void configure(Configure config) {
+        configured = true;
+
+        collection = config.getCollectionActor();
+        filePath = config.getFilePath();
+
+        //Found found = new Found(filePath, findMatches());
+        //collection.tell(found);
+    }
+
+    private void findMatches() {}
+
+    private void readFile() {}
+
 }
