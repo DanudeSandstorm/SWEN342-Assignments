d608c16dc38d757d77c29b51e81a95d2a3a776be
Daniel <dds2130@rit.edu>
2016-11-11 10:10:52 -0500

    FileCount

--- src/FileCount.java
+++ src/FileCount.java
@@ -1,4 +1,14 @@
 package src;
 
 public class FileCount {
+
+    private final int count;
+
+    public FileCount(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
 }
