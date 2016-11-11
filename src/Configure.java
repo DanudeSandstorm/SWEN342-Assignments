4cffe09109c1bb361d48c3f855aecc43c882bc60
Daniel <dds2130@rit.edu>
2016-11-11 11:31:18 -0500

    Configure object

--- src/Configure.java
+++ src/Configure.java
@@ -1,4 +1,21 @@
 package src;
 
+import akka.actor.ActorRef;
+
 public class Configure {
+    private final String filePath;
+    private final ActorRef collection;
+
+    public Configure(String filePath, ActorRef collection) {
+        this.filePath = filePath;
+        this.collection = collection;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public ActorRef getCollectionActor() {
+        return collection;
+    }
 }
