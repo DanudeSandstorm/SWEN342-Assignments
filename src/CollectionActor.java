2c236204d29202eb50bb8c5bb5107f462b1e02b9
Daniel <dds2130@rit.edu>
2016-11-11 16:39:45 -0500

    basic object and message receiving

--- src/CollectionActor.java
+++ src/CollectionActor.java
@@ -1,4 +1,36 @@
 package src;
 
-public class CollectionActor {
+import akka.actor.UntypedActor;
+
+import java.io.File;
+
+public class CollectionActor extends UntypedActor {
+    private int filesScanned;
+    private int filesToScan;
+
+    public CollectionActor() {
+        filesScanned = 0;
+        filesToScan = 0;
+    }
+
+    @Override
+    public void onReceive(Object message) throws Exception {
+        if (message instanceof FileCount) {
+            if (filesScanned == 0 && filesToScan == 0) {
+                throw new RuntimeException("File count has already been received");
+            }
+            setFileCount((FileCount) message);
+        }
+
+        if (message instanceof  Found) {
+            onFound((Found) message);
+        }
+    }
+
+    private void setFileCount(FileCount message) {
+        filesToScan = message.getCount();
+    }
+
+    private void onFound(Found found) {}
+
 }
